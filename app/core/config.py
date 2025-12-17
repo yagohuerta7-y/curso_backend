7279ff487902559e73c4f01b7a3ca5fd5a3df4c5
Yago <yagoraulhuertaorozco@gmail.com>
2025-12-16 22:33:01 -0600

    Se creó la clase Config para gestionar la ruta del archivo JSON de películas

---
+++ app/core/config.py
@@ -0,0 +1,11 @@
+# app/core
+
+class Config:
+
+    def pathJSON(self) -> str:
+        """
+        Retorna el path de nuestro JSON.
+        """
+        return r'C:\Users\yagoh\Desktop\Personal\Backend\curso_backend\app\movies.json'
+
+config = Config()
